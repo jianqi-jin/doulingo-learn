9675c805d0cfd864ae0e43959fd4e29f4a60bc51
JinJianQi <a2359634711@gmail.com>
2023-08-17 23:18:15 +0800

    feat: add google img download

---
+++ packages/img-download/src/googleimg.js
@@ -0,0 +1,55 @@
+const fetch = require("node-fetch");
+const fs = require("fs");
+const main = (url) => {
+  let pkg = "";
+  url.replace(/id=(.*?)&hl/, (match, $1) => {
+    pkg = $1;
+  });
+
+  fetch(url)
+    .then((res) => {
+      return res.text();
+    })
+    .then((res) => {
+      const list = res.split("listitem");
+      let imgs = [];
+      list.shift();
+      list.forEach((item) => {
+        item.replace(/<img src=\"(.*?)(=|\")/, (match, $1) => {
+          if (imgs.length < 3) {
+            imgs.push($1 + "=w5120-h2880-rw");
+          }
+        });
+      });
+      console.log(imgs);
+      imgs.forEach((img, idx) => {
+        fetch(img)
+          .then((res) => res.arrayBuffer())
+          .then((file) => {
+            fs.writeFile(
+              `/Users/bytedance/Desktop/素材/${pkg}_${idx}.png`,
+              Buffer.from(file),
+              (err) => {
+                console.log("download end.", err);
+              }
+            );
+          });
+      });
+    });
+};
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+rl.on('line', (line) => {
+  main(line);
+});
+
+rl.once('close', () => {
+   // end of input
+});
